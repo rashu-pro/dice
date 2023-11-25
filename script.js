1b277a6e022a276f317d24a4815afb35357c0fee
rashu-pro <rashu.pro@gmail.com>
2023-11-25 22:38:12 +0600

    updated script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import * as CANNON from 'https://cdn.skypack.dev/cannon-es';
 import * as THREE from "three";
-import * as BufferGeometryUtils from "three/addons/utils/BufferGeometryUtils.js";
+import {RoundedBoxGeometry} from "three/addons/geometries/RoundedBoxGeometry.js";
 
 const canvasEl = document.querySelector('#canvas');
 const scoreResult = document.querySelector('#score-result');
@@ -10,10 +10,7 @@ let renderer, scene, camera, diceMesh, physicsWorld;
 
 const params = {
     numberOfDice: 1,
-    segments: 40,
     edgeRadius: .1,
-    notchRadius: .15,
-    notchDepth: .09,
 };
 
 const diceArray = [];
@@ -37,8 +34,12 @@ function initScene() {
 
     scene = new THREE.Scene();
 
-    camera = new THREE.PerspectiveCamera(25, window.innerWidth / window.innerHeight, .1, 100)
-    camera.position.set(0, .4, 3.5).multiplyScalar(6);
+    //camera = new THREE.PerspectiveCamera(25, window.innerWidth / window.innerHeight, .1, 100)
+    //camera.position.set(0, .4, 3.5).multiplyScalar(6);
+    
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 100)
+    camera.position.set(0, 1, 1).multiplyScalar(6);
+    
     camera.lookAt(0, 0, 0);
 
     updateSceneSize();
@@ -97,21 +98,20 @@ function createFloor() {
 }
 
 function createDiceMesh() {
-    const boxMaterialOuter = new THREE.MeshStandardMaterial({
-        color: 0xeeeeee,
-    })
-    const boxMaterialInner = new THREE.MeshStandardMaterial({
-        color: 0x000000,
-        roughness: 0,
-        metalness: 1,
-        side: THREE.DoubleSide
-    })
+    const texLoader = new THREE.TextureLoader();
+    const boxMaterialOuter = [
+        new THREE.MeshStandardMaterial({color: 0xeeeeee, map: texLoader.load("./images/2.png")}),
+        new THREE.MeshStandardMaterial({color: 0xeeeeee, map: texLoader.load("./images/5.png")}),
+        new THREE.MeshStandardMaterial({color: 0xeeeeee, map: texLoader.load("./images/1.png")}),
+        new THREE.MeshStandardMaterial({color: 0xeeeeee, map: texLoader.load("./images/6.png")}),
+        new THREE.MeshStandardMaterial({color: 0xeeeeee, map: texLoader.load("./images/3.png")}),
+        new THREE.MeshStandardMaterial({color: 0xeeeeee, map: texLoader.load("./images/4.png")}),
+    ];
 
     const diceMesh = new THREE.Group();
-    const innerMesh = new THREE.Mesh(createInnerGeometry(), boxMaterialInner);
-    const outerMesh = new THREE.Mesh(createBoxGeometry(), boxMaterialOuter);
+    const outerMesh = new THREE.Mesh(new RoundedBoxGeometry(1, 1, 1, 5, params.edgeRadius), boxMaterialOuter);
     outerMesh.castShadow = true;
-    diceMesh.add(innerMesh, outerMesh);
+    diceMesh.add(outerMesh);
 
     return diceMesh;
 }
@@ -130,103 +130,6 @@ function createDice() {
     return {mesh, body};
 }
 
-function createBoxGeometry() {
-
-    let boxGeometry = new THREE.BoxGeometry(1, 1, 1, params.segments, params.segments, params.segments);
-
-    const positionAttr = boxGeometry.attributes.position;
-    const subCubeHalfSize = .5 - params.edgeRadius;
-
-    for (let i = 0; i < positionAttr.count; i++) {
-
-        let position = new THREE.Vector3().fromBufferAttribute(positionAttr, i);
-
-        const subCube = new THREE.Vector3(Math.sign(position.x), Math.sign(position.y), Math.sign(position.z)).multiplyScalar(subCubeHalfSize);
-        const addition = new THREE.Vector3().subVectors(position, subCube);
-
-        if (Math.abs(position.x) > subCubeHalfSize && Math.abs(position.y) > subCubeHalfSize && Math.abs(position.z) > subCubeHalfSize) {
-            addition.normalize().multiplyScalar(params.edgeRadius);
-            position = subCube.add(addition);
-        } else if (Math.abs(position.x) > subCubeHalfSize && Math.abs(position.y) > subCubeHalfSize) {
-            addition.z = 0;
-            addition.normalize().multiplyScalar(params.edgeRadius);
-            position.x = subCube.x + addition.x;
-            position.y = subCube.y + addition.y;
-        } else if (Math.abs(position.x) > subCubeHalfSize && Math.abs(position.z) > subCubeHalfSize) {
-            addition.y = 0;
-            addition.normalize().multiplyScalar(params.edgeRadius);
-            position.x = subCube.x + addition.x;
-            position.z = subCube.z + addition.z;
-        } else if (Math.abs(position.y) > subCubeHalfSize && Math.abs(position.z) > subCubeHalfSize) {
-            addition.x = 0;
-            addition.normalize().multiplyScalar(params.edgeRadius);
-            position.y = subCube.y + addition.y;
-            position.z = subCube.z + addition.z;
-        }
-
-        const notchWave = (v) => {
-            v = (1 / params.notchRadius) * v;
-            v = Math.PI * Math.max(-1, Math.min(1, v));
-            return params.notchDepth * (Math.cos(v) + 1.);
-        }
-        const notch = (pos) => notchWave(pos[0]) * notchWave(pos[1]);
-
-        const offset = .23;
-
-        if (position.y === .5) {
-            position.y -= notch([position.x, position.z]);
-        } else if (position.x === .5) {
-            position.x -= notch([position.y + offset, position.z + offset]);
-            position.x -= notch([position.y - offset, position.z - offset]);
-        } else if (position.z === .5) {
-            position.z -= notch([position.x - offset, position.y + offset]);
-            position.z -= notch([position.x, position.y]);
-            position.z -= notch([position.x + offset, position.y - offset]);
-        } else if (position.z === -.5) {
-            position.z += notch([position.x + offset, position.y + offset]);
-            position.z += notch([position.x + offset, position.y - offset]);
-            position.z += notch([position.x - offset, position.y + offset]);
-            position.z += notch([position.x - offset, position.y - offset]);
-        } else if (position.x === -.5) {
-            position.x += notch([position.y + offset, position.z + offset]);
-            position.x += notch([position.y + offset, position.z - offset]);
-            position.x += notch([position.y, position.z]);
-            position.x += notch([position.y - offset, position.z + offset]);
-            position.x += notch([position.y - offset, position.z - offset]);
-        } else if (position.y === -.5) {
-            position.y += notch([position.x + offset, position.z + offset]);
-            position.y += notch([position.x + offset, position.z]);
-            position.y += notch([position.x + offset, position.z - offset]);
-            position.y += notch([position.x - offset, position.z + offset]);
-            position.y += notch([position.x - offset, position.z]);
-            position.y += notch([position.x - offset, position.z - offset]);
-        }
-
-        positionAttr.setXYZ(i, position.x, position.y, position.z);
-    }
-
-
-    boxGeometry.deleteAttribute('normal');
-    boxGeometry.deleteAttribute('uv');
-    boxGeometry = BufferGeometryUtils.mergeVertices(boxGeometry);
-
-    boxGeometry.computeVertexNormals();
-
-    return boxGeometry;
-}
-
-function createInnerGeometry() {
-    const baseGeometry = new THREE.PlaneGeometry(1 - 2 * params.edgeRadius, 1 - 2 * params.edgeRadius);
-    const offset = .48;
-    return BufferGeometryUtils.mergeBufferGeometries([
-        baseGeometry.clone().translate(0, 0, offset),
-        baseGeometry.clone().translate(0, 0, -offset),
-        baseGeometry.clone().rotateX(.5 * Math.PI).translate(0, -offset, 0),
-        baseGeometry.clone().rotateX(.5 * Math.PI).translate(0, offset, 0),
-        baseGeometry.clone().rotateY(.5 * Math.PI).translate(-offset, 0, 0),
-        baseGeometry.clone().rotateY(.5 * Math.PI).translate(offset, 0, 0),
-    ], false);
-}
 
 function addDiceEvents(dice) {
     dice.body.addEventListener('sleep', (e) => {
